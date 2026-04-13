dad1940b1bcd5115a1270b1fffdfcd522074df68
edomusu1991 <edomusu91@gmail.com>
2026-04-13 19:01:40 +0200

    Fix AuthenticationManager bean

--- src/main/java/com/risiko/config/SecurityConfig.java
+++ src/main/java/com/risiko/config/SecurityConfig.java
@@ -12,6 +12,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +27,11 @@ public class SecurityConfig {
 
     public SecurityConfig(@Lazy JwtFilter jwtFilter) {
         this.jwtFilter = jwtFilter;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
